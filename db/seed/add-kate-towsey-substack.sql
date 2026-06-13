1889111927157fb00f165e528c94c163c79ad8ec
czhengjuarez <czheng.juarez@gmail.com>
2026-06-13 13:46:31 -0400

    Add Kate Towsey's Substack to Research category

---
+++ db/seed/add-kate-towsey-substack.sql
@@ -0,0 +1,12 @@
+-- Add Kate Towsey's Substack to Research (cat 11)
+INSERT INTO resources (title, description, url, category_id, type, tags, source, status)
+VALUES (
+  'Kate Towsey''s Substack',
+  'Newsletter on ResearchOps from Kate Towsey, ResearchOps practitioner and author',
+  'https://substack.com/@katetowsey',
+  11,
+  'article',
+  '["researchops","newsletter"]',
+  'manual',
+  'published'
+);
